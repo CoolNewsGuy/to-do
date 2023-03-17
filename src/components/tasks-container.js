0d81aeb71f6735b4c51f234530a1b76b15ee1697
The Wonderful Cat <joewilliamdd31@gmail.com>
2023-03-17 22:50:56 +0100

    Create tasks container component

---
+++ src/components/tasks-container.js
@@ -0,0 +1,11 @@
+import "./tasks-container.css";
+
+function TasksContainer() {
+	const container = document.createElement("div");
+
+	container.classList.add("tasks-container");
+
+	return container;
+}
+
+export default TasksContainer;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "../normalize.css";
 import "../style.css";
+
+import TasksContainer from "./components/tasks-container";
+
+document.body.appendChild(TasksContainer());

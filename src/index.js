7f8ced47dc9103568fa828ee329e130723d6b29c
The Wonderful Cat <joewilliamdd31@gmail.com>
2023-03-18 13:43:11 +0100

    Append the top container to the tasks container

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import "../normalize.css";
 import "../style.css";
 
 import TasksContainer from "./components/tasks-container/tasks-container";
-import TaskBar from "./components/task-bar/task-bar";
+import TopContainer from "./components/top-taskbar-container/container";
 
 document.body.appendChild(TasksContainer());
-document.querySelector(".tasks-container").append(TaskBar());
+document.querySelector(".tasks-container").append(TopContainer);

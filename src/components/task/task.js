961a442f7ce8a5e592482d409f89fb7b035d2af8
The Wonderful Cat <joewilliamdd31@gmail.com>
2023-03-22 10:07:29 +0100

    Create a container for the trash icon and style it

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,9 +1,12 @@
 import "./task.css";
+import "./trash.css";
+import TrashHTML from "../../assets/trash.svg";
 
 function Task() {
 	let taskDiv = document.createElement("div");
 
 	taskDiv.classList.add("task");
+	taskDiv.append(TaskText("Hello world"), TaskDeleteIcon());
 
 	return taskDiv;
 }
@@ -15,6 +18,16 @@ function TaskText(text) {
 	taskP.textContent = text;
 
 	return taskP;
+}
+
+function TaskDeleteIcon() {
+	let iconContainer = document.createElement("object");
+
+	iconContainer.classList.add("trash-container");
+	iconContainer.data = TrashHTML;
+	iconContainer.type = "image/svg+xml";
+
+	return iconContainer;
 }
 
 export default Task;

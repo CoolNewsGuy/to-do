cbe3b3918f6632464054fab9b75db1e287df3618
The Wonderful Cat <joewilliamdd31@gmail.com>
2023-03-23 07:05:37 +0100

    Add a hover effect for trash button

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -3,31 +3,45 @@ import "./trash.css";
 import TrashHTML from "../../assets/trash.svg";
 
 function Task() {
-	let taskDiv = document.createElement("div");
+   let taskDiv = document.createElement("div");
 
-	taskDiv.classList.add("task");
-	taskDiv.append(TaskText("Hello world"), TaskDeleteIcon());
+   taskDiv.classList.add("task");
+   taskDiv.append(TaskText("Hello world"), TaskDeleteIcon());
 
-	return taskDiv;
+   return taskDiv;
 }
 
 function TaskText(text) {
-	let taskP = document.createElement("p");
+   let taskP = document.createElement("p");
 
-	taskP.classList.add("task-text");
-	taskP.textContent = text;
+   taskP.classList.add("task-text");
+   taskP.textContent = text;
 
-	return taskP;
+   return taskP;
 }
 
 function TaskDeleteIcon() {
-	let iconContainer = document.createElement("object");
+   // will be used to help us change the color of trash when hovering
+   let iconContainer = document.createElement("object");
+   iconContainer.classList.add("trash-container");
+   iconContainer.data = TrashHTML;
+   iconContainer.type = "image/svg+xml";
 
-	iconContainer.classList.add("trash-container");
-	iconContainer.data = TrashHTML;
-	iconContainer.type = "image/svg+xml";
+   // for hover effect
+   function changeIconStrokeColor(color) {
+      const trashIcon = iconContainer.contentDocument.querySelector("svg path");
+      trashIcon.setAttribute("stroke", color);
+   }
 
-	return iconContainer;
+   iconContainer.addEventListener("mouseover", () => {
+      changeIconStrokeColor("#07f");
+   });
+
+   iconContainer.addEventListener("mouseleave", () => {
+      changeIconStrokeColor("#fff");
+   });
+
+   return iconContainer;
 }
 
 export default Task;

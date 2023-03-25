76b9ce8dcc484f56f8a398f1ab793ccfb0b5485a
The Wonderful Cat <joewilliamdd31@gmail.com>
2023-03-25 02:04:59 +0100

    Make the add button functional

--- src/components/top-taskbar-container/add-task-btn/add-task-btn.js
+++ src/components/top-taskbar-container/add-task-btn/add-task-btn.js
@@ -1,12 +1,24 @@
 import "./add-task-btn.css";
+import Task from "../../task/task";
+import TasksList from "../../tasks-list/tasks-list";
 
 function AddTaskBtn() {
-	let addBtn = document.createElement("button");
+   let addBtn = document.createElement("button");
 
-	addBtn.textContent = "ADD";
-	addBtn.classList.add("add-task-btn");
+   addBtn.textContent = "ADD";
+   addBtn.classList.add("add-task-btn");
+   addBtn.onclick = addTask;
 
-	return addBtn;
+   return addBtn;
+}
+
+function addTask() {
+   let taskText = document.querySelector(".task-bar").value;
+
+   if (taskText) {
+      let newTask = Task(taskText);
+      TasksList.append(newTask);
+   }
 }
 
 export default AddTaskBtn;

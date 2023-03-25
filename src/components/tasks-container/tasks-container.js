f6b9e325945f16295928812fafb7e2a10c5f7111
The Wonderful Cat <joewilliamdd31@gmail.com>
2023-03-25 01:38:49 +0100

    Make index.js shorter and append top container and tasks list in tasks-container component

--- src/components/tasks-container/tasks-container.js
+++ src/components/tasks-container/tasks-container.js
@@ -1,11 +1,15 @@
 import "./tasks-container.css";
+import TopContainer from "../top-taskbar-container/top-container";
+import TasksList from "../tasks-list/tasks-list";
 
-function TasksContainer() {
-	const container = document.createElement("div");
+let TasksContainer = (() => {
+   const container = document.createElement("div");
 
-	container.classList.add("tasks-container");
+   container.classList.add("tasks-container");
 
-	return container;
-}
+   return container;
+})();
+
+TasksContainer.append(TopContainer, TasksList);
 
 export default TasksContainer;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import "../normalize.css";
 import "../style.css";
 
 import TasksContainer from "./components/tasks-container/tasks-container";
-import TopContainer from "./components/top-taskbar-container/container";
 
-document.body.appendChild(TasksContainer());
-document.querySelector(".tasks-container").append(TopContainer);
+document.body.appendChild(TasksContainer);
